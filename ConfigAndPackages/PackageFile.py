d09f71faf07418e7f33ac71e50c05299ba5975a9
siavooshpayandehazad <siavoosh@siavooshs-MacBook-Pro.local>
2015-06-26 10:27:56 +0300

    fixed North Last turn model

--- ConfigAndPackages/PackageFile.py
+++ ConfigAndPackages/PackageFile.py
@@ -8,7 +8,7 @@ XY_TurnModel = ['E2N', 'E2S', 'W2N', 'W2S']
 # at the moment there is no support adaptive routing algorithm
 # These are just here...
 WestFirst_TurnModel = ['E2N', 'E2S', 'W2N', 'W2S', 'S2E', 'N2E']
-NorthLast_TurnModel = ['E2N', 'E2S', 'W2N', 'W2S', 'S2W', 'S2E']
+NorthLast_TurnModel = ['E2N', 'E2S', 'W2N', 'W2S', 'N2W', 'N2E']
 
 ################################################
 #          SHM Sets
