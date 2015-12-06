6e7dd76b092406bbb4c5dc0b75d2ed74271de5c7
Siavoosh Payandeh Azad <siavooshpayandeh@gmail.com>
2015-12-06 23:32:14 +0200

    code cleanup and documentation

--- src/main/python/Simulator/FaultInjector.py
+++ src/main/python/Simulator/FaultInjector.py
@@ -9,10 +9,22 @@ from SystemHealthMonitoring import SHMU_Functions
 #
 ####################################################################
 
-def FaultEvent(env, AG, SHMU, NoCRG, FaultTimeList, counter_threshold, logging):
+
+def FaultEvent(env, AG, SHMU, NoCRG, fault_time_list, counter_threshold, logging):
+    """
+
+    :param env: Simulator Environment
+    :param AG: Architecture Graph
+    :param SHMU: System health Monitoring Unit
+    :param NoCRG: NoC routing Graph
+    :param fault_time_list: List of faults to happen in future
+    :param counter_threshold: counter threshold object
+    :param logging: logging file
+    :return:
+    """
     Fault = False
     while True:
-        for FaultTime in FaultTimeList:
+        for FaultTime in fault_time_list:
             #print env.now, FaultTime
             if float("{0:.1f}".format(env.now)) == FaultTime:
                 Fault = True
--- src/main/python/Simulator/Simulator.py
+++ src/main/python/Simulator/Simulator.py
@@ -9,112 +9,128 @@ from FaultInjector import FaultEvent
 from SystemHealthMonitoring.FaultClassifier import CounterThreshold
 
 
-def Processor(env, Node, Schedule, counter_threshold, logging):
+def processor_sim(env, node, schedule, counter_threshold, logging):
     """
     Runs tasks on each node
     :param env: simulation environment
-    :param Node: Node ID number
-    :param Schedule: schedule of the tasks on the Node
-    :return: none
+    :param node: Node ID number
+    :param schedule: schedule of the tasks on the Node
+    :param counter_threshold: counter threshold object
+    :param logging: logging file
+    :return:
     """
-    Found = False
-    TaskNum = None
+    found = False
+    task_num = None
     length = 0
     while True:
-        for key in Schedule.keys():
-            if env.now == Schedule[key][0]:
-                print float("{0:.1f}".format(env.now)), "\tNODE:: Found Task", key, " to run on Node:", Node
-                length = Schedule[key][1]-Schedule[key][0]
-                Found = True
-                TaskNum = key
+        for key in schedule.keys():
+            if env.now == schedule[key][0]:
+                print float("{0:.1f}".format(env.now)), "\tNODE:: Found Task", key, " to run on Node:", node
+                length = schedule[key][1]-schedule[key][0]
+                found = True
+                task_num = key
                 break
-        if Found:
-            print float("{0:.1f}".format(env.now)), "\tNODE:: Starting Task", TaskNum, "on Node:", Node
-            counter_threshold.decrease_counter(Node, logging)
+        if found:
+            print float("{0:.1f}".format(env.now)), "\tNODE:: Starting Task", task_num, "on Node:", node
+            counter_threshold.decrease_counter(node, logging)
             yield env.timeout(length)
-            print float("{0:.1f}".format(env.now)), "\tNODE:: Task", TaskNum, "execution finished on Node", Node
-            Found = False
-        else:
-            yield env.timeout(1)
-
-def Router(env, Node, Schedule, counter_threshold, logging):
-    Found = False
-    TaskNum = None
-    length = 0
-    #print "HERE:",Schedule
-    while True:
-
-        for key in Schedule.keys():
-            if env.now == Schedule[key][0][0]:
-                print float("{0:.1f}".format(env.now)), "\tRouter:: Found Task", key, " to run on Router:", Node
-                length = Schedule[key][0][1]-Schedule[key][0][0]
-                Found = True
-                TaskNum = key
-                break
-        if Found:
-            print float("{0:.1f}".format(env.now)), "\tRouter:: Starting Task", TaskNum, "on Router:", Node
-            dict = {Node: "R"}
-            counter_threshold.decrease_counter(dict, logging)
-            yield env.timeout(length)
-            print float("{0:.1f}".format(env.now)), "\tRouter:: Task", TaskNum, "execution finished on Router", Node
-            Found = False
+            print float("{0:.1f}".format(env.now)), "\tNODE:: Task", task_num, "execution finished on Node", node
+            found = False
         else:
             yield env.timeout(1)
 
 
-def Link(env, Link, Schedule, counter_threshold, logging):
+def router_sim(env, node, schedule, counter_threshold, logging):
+    """
+    runs tasks on the routers
+    :param env: simulation environment
+    :param node: ID of the node to be simulated
+    :param schedule: schedule of the tasks on the Router
+    :param counter_threshold: counter threshold object
+    :param logging: logging file
+    :return:
+    """
+    found = False
+    task_num = None
+    length = 0
+    # print "HERE:",Schedule
+    while True:
+
+        for key in schedule.keys():
+            if env.now == schedule[key][0][0]:
+                print float("{0:.1f}".format(env.now)), "\tRouter:: Found Task", key, " to run on Router:", node
+                length = schedule[key][0][1]-schedule[key][0][0]
+                found = True
+                task_num = key
+                break
+        if found:
+            print float("{0:.1f}".format(env.now)), "\tRouter:: Starting Task", task_num, "on Router:", node
+            location_dict = {node: "R"}
+            counter_threshold.decrease_counter(location_dict, logging)
+            yield env.timeout(length)
+            print float("{0:.1f}".format(env.now)), "\tRouter:: Task", task_num, "execution finished on Router", node
+            found = False
+        else:
+            yield env.timeout(1)
+
+
+def link_sim(env, link, schedule, counter_threshold, logging):
     """
     Runs tasks on each link
     :param env: simulation environment
-    :param Link: Link number
-    :param Schedule: schedule of the tasks on the link
-    :return: none
+    :param link: link number
+    :param schedule: schedule of the tasks on the link
+    :param counter_threshold: counter threshold object
+    :param logging: logging file
+    :return:
     """
-    Found = False
-    TaskNum = None
+    found = False
+    task_num = None
     length = 0
     while True:
-        for key in Schedule.keys():
-            if env.now == Schedule[key][0][0]:
-                print float("{0:.1f}".format(env.now)), "\tLINK:: Found Task", key, " to run on Link:", Link
-                length = Schedule[key][0][1]-Schedule[key][0][0]
-                Found = True
-                TaskNum = key
-        if Found:
-            print float("{0:.1f}".format(env.now)), "\tLINK:: Starting Task", TaskNum, "on Link:", Link
-            counter_threshold.decrease_counter(Link, logging)
+        for key in schedule.keys():
+            if env.now == schedule[key][0][0]:
+                print float("{0:.1f}".format(env.now)), "\tLINK:: Found Task", key, " to run on Link:", link
+                length = schedule[key][0][1]-schedule[key][0][0]
+                found = True
+                task_num = key
+        if found:
+            print float("{0:.1f}".format(env.now)), "\tLINK:: Starting Task", task_num, "on Link:", link
+            counter_threshold.decrease_counter(link, logging)
             yield env.timeout(length)
-            print float("{0:.1f}".format(env.now)), "\tLINK:: Task", TaskNum, "execution finished on Link", Link
-            Found = False
+            print float("{0:.1f}".format(env.now)), "\tLINK:: Task", task_num, "execution finished on Link", link
+            found = False
         else:
             yield env.timeout(1)
 
 
-def RunSimulator(Runtime, AG, SHM, NoCRG, logging):
+
+def RunSimulator(runtime, AG, SHM, NoCRG, logging):
     print "==========================================="
     print "STARTING SIMULATION..."
     env = simpy.Environment()
     counter_threshold = CounterThreshold.CounterThreshold(4)
     for node in AG.nodes():
         # print node, AG.node[node]["Scheduling"]
-        env.process(Processor(env, node, AG.node[node]['PE'].Scheduling, counter_threshold, logging))
-        env.process(Router(env, node, AG.node[node]['Router'].Scheduling, counter_threshold, logging))
+        env.process(processor_sim(env, node, AG.node[node]['PE'].Scheduling, counter_threshold, logging))
+        env.process(router_sim(env, node, AG.node[node]['Router'].Scheduling, counter_threshold, logging))
     for link in AG.edges():
         # print link, AG.edge[link[0]][link[1]]["Scheduling"]
-        env.process(Link(env, link, AG.edge[link[0]][link[1]]["Scheduling"], counter_threshold, logging))
+        env.process(link_sim(env, link, AG.edge[link[0]][link[1]]["Scheduling"], counter_threshold, logging))
 
-    FaultTimeList = []
-    FaultTime = 0
+    fault_time_list = []
+    fault_time = 0
     if Config.EventDrivenFaultInjection:
         TimeUntilNextFault = numpy.random.normal(Config.MTBF, Config.SD4MTBF)
-        FaultTime += TimeUntilNextFault
-        while FaultTime < Runtime:
-            FaultTimeList.append(float("{0:.1f}".format(FaultTime)))
+        fault_time += TimeUntilNextFault
+        while fault_time < runtime:
+            fault_time_list.append(float("{0:.1f}".format(fault_time)))
             TimeUntilNextFault = numpy.random.normal(Config.MTBF, Config.SD4MTBF)
-            FaultTime += TimeUntilNextFault
-        print FaultTimeList
-        env.process(FaultEvent(env, AG, SHM, NoCRG, FaultTimeList, counter_threshold, logging))
+            fault_time += TimeUntilNextFault
+        print fault_time_list
+        env.process(FaultEvent(env, AG, SHM, NoCRG, fault_time_list, counter_threshold, logging))
 
-    env.run(until=Runtime)
+    env.run(until=runtime)
     print "DEAD Components:", counter_threshold.dead_components
     print "SIMULATION FINISHED..."
+    return None
