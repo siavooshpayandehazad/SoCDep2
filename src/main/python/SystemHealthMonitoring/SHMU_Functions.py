d82ae4b5b759d6848cd048fb4c52adc7c1ca1a0b
Siavoosh Payandeh Azad <siavooshpayandeh@gmail.com>
2015-12-12 15:42:24 +0200

    cleanup
    preparing for going for configparser :D

--- src/main/python/SystemHealthMonitoring/SHMU_Functions.py
+++ src/main/python/SystemHealthMonitoring/SHMU_Functions.py
@@ -5,24 +5,24 @@ import SHMU_Reports
 from RoutingAlgorithms import Routing
 
 
-def ApplyInitialFaults(SHM):
-        for BrokenLink in Config.ListOfBrokenLinks:
-            SHM.break_link(BrokenLink, True)
+def ApplyInitialFaults(shm):
+        for broken_link in Config.ListOfBrokenLinks:
+            shm.break_link(broken_link, True)
 
-        for NodeWithBrokenTurn in Config.ListOfBrokenTurns:
-            SHM.break_turn(NodeWithBrokenTurn, Config.ListOfBrokenTurns[NodeWithBrokenTurn], True)
+        for router_with_broken_turn in Config.ListOfBrokenTurns:
+            shm.break_turn(router_with_broken_turn, Config.ListOfBrokenTurns[router_with_broken_turn], True)
 
-        for AgedPE in Config.ListOfAgedPEs:
-            SHM.introduce_aging(AgedPE, Config.ListOfAgedPEs[AgedPE], True)
+        for aged_pe in Config.ListOfAgedPEs:
+            shm.introduce_aging(aged_pe, Config.ListOfAgedPEs[aged_pe], True)
 
-        for BrokenNode in Config.ListOfBrokenPEs:
-            SHM.break_node(BrokenNode, True)
+        for broken_node in Config.ListOfBrokenPEs:
+            shm.break_node(broken_node, True)
 
 
-def RandomFaultGeneration(SHM):
+def RandomFaultGeneration(shm):
     """
 
-    :param SHM: System Health Map
+    :param shm: System Health Map
     :return:
     """
     ChosenFault = random.choice(['Link', 'Turn', 'Node'])
@@ -31,32 +31,33 @@ def RandomFaultGeneration(SHM):
     FaultTypes = ['T']
     FaultType = random.choice(FaultTypes)
     if ChosenFault == 'Link':
-        ChosenLink = random.choice(SHM.edges())
+        ChosenLink = random.choice(shm.edges())
         return ChosenLink, FaultType
     elif ChosenFault == 'Turn':
-        ChosenNode = random.choice(SHM.nodes())
-        ChosenTurn = random.choice(SHM.node[ChosenNode]['TurnsHealth'].keys())
+        ChosenNode = random.choice(shm.nodes())
+        ChosenTurn = random.choice(shm.node[ChosenNode]['TurnsHealth'].keys())
         while not ChosenTurn in Config.UsedTurnModel:
-            ChosenTurn = random.choice(SHM.node[ChosenNode]['TurnsHealth'].keys())
+            ChosenTurn = random.choice(shm.node[ChosenNode]['TurnsHealth'].keys())
         return {ChosenNode: ChosenTurn}, FaultType
     elif ChosenFault == 'Node':
-        ChosenNode = random.choice(SHM.nodes())
+        ChosenNode = random.choice(shm.nodes())
         return ChosenNode, FaultType
 
 
-def GenerateFaultConfig (SHMU):
+def GenerateFaultConfig(shmu):
     """
-    Generates a string (FaultConfig) from the configuration of the faults in the SHM
-    :return: FaultConfig string
+    Generates a string (fault_config) from the configuration of the faults in the SHM
+    :param shmu: System Health Monitoring Unit
+    :return: fault_config string
     """
-    FaultConfig = ""
-    for node in SHMU.SHM.nodes():
-        FaultConfig += str(node)
-        FaultConfig += "T" if SHMU.SHM.node[node]['NodeHealth'] else "F"
-        FaultConfig += str(int(SHMU.SHM.node[node]['NodeSpeed']))
-        for Turn in SHMU.SHM.node[node]['TurnsHealth']:
-            FaultConfig += "T" if SHMU.SHM.node[node]['TurnsHealth'][Turn] else "F"
-    return FaultConfig
+    fault_config = ""
+    for node in shmu.SHM.nodes():
+        fault_config += str(node)
+        fault_config += "T" if shmu.SHM.node[node]['NodeHealth'] else "F"
+        fault_config += str(int(shmu.SHM.node[node]['NodeSpeed']))
+        for Turn in shmu.SHM.node[node]['TurnsHealth']:
+            fault_config += "T" if shmu.SHM.node[node]['TurnsHealth'][Turn] else "F"
+    return fault_config
 
 
 def apply_fault_event(AG, SHMU, NoCRG, FaultLocation, FaultType):
