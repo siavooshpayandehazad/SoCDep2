3d84430f66e9c6ff1327170795aa453c414ad249
Siavoosh Payandeh Azad <siavooshpayandeh@gmail.com>
2016-01-19 15:29:56 +0100

    counter-viz update

--- src/main/python/SystemHealthMonitoring/FaultClassifier/Counter_Threshold_Viz.py
+++ src/main/python/SystemHealthMonitoring/FaultClassifier/Counter_Threshold_Viz.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2015 Siavoosh Payandeh Azad
 import matplotlib.pyplot as plt
 from ConfigAndPackages import Config
-import numpy
+
 
 def counter_threshold_viz(ag, counter_threshold):
     """
@@ -10,77 +10,92 @@ def counter_threshold_viz(ag, counter_threshold):
     :param counter_threshold: counter_threshold object
     :return: None
     """
-    num_of_plots = len(counter_threshold.counters_f_report) * 3
-    width = min(len(counter_threshold.counters_f_report["0"])/10, 32768)
-    # print width, num_of_plots
-    fig = plt.figure(figsize=(width, num_of_plots))
-    plt.subplots_adjust(hspace=0.5)
+    width = len(counter_threshold.viz_counter_list["0"])+2
+    fig = plt.figure(figsize=(width/10 + 2, 200))
+    plt.subplots_adjust(wspace=0.9, hspace=0.9)
     max_threshold_value = max(Config.fault_counter_threshold, Config.health_counter_threshold,
-                              Config.intermittent_counter_threshold)
+                              Config.intermittent_counter_threshold)+0.05
     count = 0
+    # for Processing elements
     for node in ag.nodes():
 
         location = str(node)
         length = len(counter_threshold.counters_f_report[location])
+        last_number = counter_threshold.viz_counter_list[location][-1]
         count += 1
         # we add the graphs one by one, the Health graph is at the back, then Fault graph and then intermittent
         # graph in front.
-        ax1 = fig.add_subplot(length, 1, count)
-        ax1.fill_between(counter_threshold.viz_counter_list[location]+[length, length],
+        ax1 = fig.add_subplot(length+2, 1, count)
+        # Generating health monitor counters visualization
+        ax1.fill_between(counter_threshold.viz_counter_list[location]+[last_number, last_number],
                          counter_threshold.counters_h_report[location]+[0, max_threshold_value], 0, facecolor='g')
-        ax1.fill_between(counter_threshold.viz_counter_list[location]+[length, length],
+        # Generating fault monitor counters visualization
+        ax1.fill_between(counter_threshold.viz_counter_list[location]+[last_number, last_number],
                          counter_threshold.counters_f_report[location]+[0, max_threshold_value], 0, facecolor='r')
-        ax1.fill_between(counter_threshold.viz_counter_list[location]+[length, length],
+        # Generating intermittent monitor counters visualization
+        ax1.fill_between(counter_threshold.viz_counter_list[location]+[last_number, last_number],
                          counter_threshold.counters_i_report[location]+[0, max_threshold_value], 0, edgecolor='k')
+        # disable x and y axis ticks
         plt.setp(ax1.get_xticklabels(), visible=False)
         plt.setp(ax1.get_yticklabels(), visible=False)
         # move the y labels away
         ax1.yaxis.set_label_coords(-0.01, 0)
         ax1.set_ylabel(r'PE'+str(node), size=14, rotation=0)
         # removes the extra white space at the end of the graph
-        ax1.set_xlim(0, width+1)
+        ax1.set_xlim(0, width+0.1)
 
+    # For Routers
     for node in ag.nodes():
         location = "R"+str(node)
         length = len(counter_threshold.counters_f_report[location])
+        last_number = counter_threshold.viz_counter_list[location][-1]
         count += 1
-        ax1 = fig.add_subplot(length, 1, count)
-        ax1.fill_between(counter_threshold.viz_counter_list[location]+[length, length],
+        ax1 = fig.add_subplot(length+2, 1, count)
+        # Generating health monitor counters visualization
+        ax1.fill_between(counter_threshold.viz_counter_list[location]+[last_number, last_number],
                          counter_threshold.counters_h_report[location]+[0, max_threshold_value], 0, facecolor='g')
-        ax1.fill_between(counter_threshold.viz_counter_list[location]+[length, length],
+        # Generating fault monitor counters visualization
+        ax1.fill_between(counter_threshold.viz_counter_list[location]+[last_number, last_number],
                          counter_threshold.counters_f_report[location]+[0, max_threshold_value], 0, facecolor='r')
-        ax1.fill_between(counter_threshold.viz_counter_list[location]+[length, length],
+        # Generating intermittent monitor counters visualization
+        ax1.fill_between(counter_threshold.viz_counter_list[location]+[last_number, last_number],
                          counter_threshold.counters_i_report[location]+[0, max_threshold_value], 0, edgecolor='k')
+        # disable x and y axis ticks
         plt.setp(ax1.get_xticklabels(), visible=False)
         plt.setp(ax1.get_yticklabels(), visible=False)
         ax1.set_ylabel(r'R'+str(node), size=14, rotation=0)
-         # move the y labels away
+        # move the y labels away
         ax1.yaxis.set_label_coords(-0.01, 0)
         # removes the extra white space at the end of the graph
-        ax1.set_xlim(0, width+1)
+        ax1.set_xlim(0, width+0.1)
 
+    # for physical links
     link_counter = 0
     for link in ag.edges():
         location = "L"+str(link[0])+str(link[1])
         length = len(counter_threshold.counters_f_report[location])
+        last_number = counter_threshold.viz_counter_list[location][-1]
         count += 1
         link_counter += 1
-        ax1 = fig.add_subplot(length, 1, count)
-        ax1.fill_between(counter_threshold.viz_counter_list[location]+[length, length],
+        ax1 = fig.add_subplot(length+2, 1, count)
+        # Generating health monitor counters visualization
+        ax1.fill_between(counter_threshold.viz_counter_list[location]+[last_number, last_number],
                          counter_threshold.counters_h_report[location]+[0, max_threshold_value], 0, facecolor='g')
-        ax1.fill_between(counter_threshold.viz_counter_list[location]+[length, length],
+        # Generating fault monitor counters visualization
+        ax1.fill_between(counter_threshold.viz_counter_list[location]+[last_number, last_number],
                          counter_threshold.counters_f_report[location]+[0, max_threshold_value], 0, facecolor='r')
-        ax1.fill_between(counter_threshold.viz_counter_list[location]+[length, length],
+        # Generating intermittent monitor counters visualization
+        ax1.fill_between(counter_threshold.viz_counter_list[location]+[last_number, last_number],
                          counter_threshold.counters_i_report[location]+[0, max_threshold_value], 0, edgecolor='k')
+        # disable x and y axis ticks
         plt.setp(ax1.get_yticklabels(), visible=False)
-        if link_counter != len(ag.edges()):
-            plt.setp(ax1.get_xticklabels(), visible=False)
+        plt.setp(ax1.get_xticklabels(), visible=False)
         # move the y labels away
         ax1.yaxis.set_label_coords(-0.01, 0)
         ax1.set_ylabel(location, size=14, rotation=0)
         # removes the extra white space at the end of the graph
-        ax1.set_xlim(0, width+1)
+        ax1.set_xlim(0, width+0.1)
 
-    plt.savefig("GraphDrawings/Counter_Threshold_Viz.png", dpi=200, bbox_inches='tight')
+    plt.savefig("GraphDrawings/Counter_Threshold_Viz.png", dpi=120, bbox_inches='tight')
     plt.clf()
     plt.close(fig)
