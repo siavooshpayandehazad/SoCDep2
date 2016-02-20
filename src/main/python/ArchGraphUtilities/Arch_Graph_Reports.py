42d9a935cb20af14df564bb83be8212071454339
Siavoosh Payandeh Azad <siavooshpayandeh@gmail.com>
2016-02-20 21:43:36 +0200

    AG drawing update
    The idea is that this drawing thingy should scale. previously it was
    messing up the whole thing above 5x5x5. now i tested until 10x10x10 and
    it works!

--- src/main/python/ArchGraphUtilities/Arch_Graph_Reports.py
+++ src/main/python/ArchGraphUtilities/Arch_Graph_Reports.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import networkx
 from ConfigAndPackages import Config
 from AG_Functions import return_node_location
+import math
+
 
 
 def draw_ag(ag, file_name):
@@ -17,17 +19,22 @@ def draw_ag(ag, file_name):
     color_list = []
 
     number_of_layers = Config.Network_Z_Size
-    node_size = 700/number_of_layers
+    largest_number = Config.Network_X_Size*Config.Network_Y_Size*Config.Network_Z_Size -1
+    node_size = math.log10(largest_number)*60
+    # print "Node Size", node_size
 
-    node_distance_x = (6 * node_size * Config.Network_X_Size * (number_of_layers+1))
-    node_distance_y = (6 * node_size * Config.Network_Y_Size * (number_of_layers+1))
+    node_distance_x = 0.2 * (number_of_layers+1)
+    node_distance_y = 0.2 * (number_of_layers+1)
 
-    offset_x = (6 * node_size * Config.Network_X_Size)
-    offset_y = (6 * node_size * Config.Network_Y_Size)
+    offset_x = 0.2
+    offset_y = 0.2
+
+    plt.figure(num=None, figsize=(3*Config.Network_X_Size, 3*Config.Network_Y_Size), dpi=350, facecolor='w', edgecolor='k')
 
     for Node in ag.nodes():
         x, y, z = return_node_location(Node)
-        position[Node] = [(x*node_distance_x)+z*offset_x, (y*node_distance_y)+z*offset_y]
+        position[Node] = [(x*node_distance_x)+(z*offset_x), (y*node_distance_y)+(z*offset_y)]
+        # print (x*node_distance_x)+(z*offset_x), (y*node_distance_y)+(z*offset_y)
         if ag.node[Node]['Region'] == 'H':
             color_list.append('#FF878B')
         elif ag.node[Node]['Region'] == 'GH':   # gateway to high critical
@@ -40,7 +47,7 @@ def draw_ag(ag, file_name):
     # POS = networkx.spring_layout(AG)
 
     networkx.draw(ag, pos=position, with_labels=True, node_size=node_size, arrows=False,
-                  node_color=color_list, font_size=5, linewidths=1)
-    plt.savefig("GraphDrawings/"+file_name+".png", dpi=150)
+                  node_color=color_list, font_size=7, linewidths=1)
+    plt.savefig("GraphDrawings/"+file_name+".png")
     plt.clf()
     return None
